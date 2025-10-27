22274f5c8148baa0c2ccc61ea2b29771bf748f8a
Shrisha22 <Shrissham@gmail.com>
2025-10-27 08:29:51 +0530

    This is related to subjects

---
+++ subjects.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+   char subjects[20];
+    printf("Enter the name of subjects:");
+    for(int i=0;i<subjects;i++){
+        scanf("%s",subjects[i]);
+    }
+    printf("The subjects are:");
+    for(int i=0;i<subjects;i++){
+        printf("The subjects are:",subjects[i];)
+    }
+}
